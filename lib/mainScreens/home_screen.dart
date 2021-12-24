037db7ba36fe68c0dea857a1c3eb6b0af77f2617
emorilebo <emorylebo@gmail.com>
2021-12-24 11:04:33 +0100

    ADD home screen

---
+++ lib/mainScreens/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: MyDrawer(),
+      appBar: AppBar(
+        title: Text(
+          sharedPreferences!.getString("name")!,
+          style: const TextStyle(fontSize: 30, fontFamily: 'Lobster'),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (c) => const MenusUploadScreen()));
+            },
+            icon: const Icon(Icons.post_add, color: Colors.white),
+          ),
+        ],
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.cyan,
+                Colors.amber,
+              ],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+      ),
+      body: Center(),
+    );
+  }
+}
