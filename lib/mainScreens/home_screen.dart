bd4a567b2eb690efb580f1491e4957b500d5f88d
emorilebo <emorylebo@gmail.com>
2021-12-29 09:26:55 +0100

    UPDATE Homescreen with map

--- lib/mainScreens/home_screen.dart
+++ lib/mainScreens/home_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:engapp__beta/global/global.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,42 +12,64 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController? newGoogleMapController;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // drawer: MyDrawer(),
-        // appBar: AppBar(
-        //   title: Text(
-        //     sharedPreferences!.getString("name")!,
-        //     style: const TextStyle(fontSize: 30, fontFamily: 'Lobster'),
-        //   ),
-        //   centerTitle: true,
-        //   automaticallyImplyLeading: true,
-        //   actions: [
-        //     IconButton(
-        //       onPressed: () {
-        //         // Navigator.push(context,
-        //         //     MaterialPageRoute(builder: (c) => const MenusUploadScreen()));
-        //       },
-        //       icon: const Icon(Icons.post_add, color: Colors.white),
-        //     ),
-        //   ],
-        //   flexibleSpace: Container(
-        //     decoration: const BoxDecoration(
-        //       gradient: LinearGradient(
-        //         colors: [
-        //           Colors.cyan,
-        //           Colors.amber,
-        //         ],
-        //         begin: FractionalOffset(0.0, 0.0),
-        //         end: FractionalOffset(1.0, 0.0),
-        //         stops: [0.0, 1.0],
-        //         tileMode: TileMode.clamp,
-        //       ),
-        //     ),
-        //   ),
-        // ),
-        // body: Center(),
-        );
+      // drawer: MyDrawer(),
+      // appBar: AppBar(
+      //   title: Text(
+      //     sharedPreferences!.getString("name")!,
+      //     style: const TextStyle(fontSize: 30, fontFamily: 'Lobster'),
+      //   ),
+      //   centerTitle: true,
+      //   automaticallyImplyLeading: true,
+      //   actions: [
+      //     IconButton(
+      //       onPressed: () {
+      //         // Navigator.push(context,
+      //         //     MaterialPageRoute(builder: (c) => const MenusUploadScreen()));
+      //       },
+      //       icon: const Icon(Icons.post_add, color: Colors.white),
+      //     ),
+      //   ],
+      //   flexibleSpace: Container(
+      //     decoration: const BoxDecoration(
+      //       gradient: LinearGradient(
+      //         colors: [
+      //           Colors.cyan,
+      //           Colors.amber,
+      //         ],
+      //         begin: FractionalOffset(0.0, 0.0),
+      //         end: FractionalOffset(1.0, 0.0),
+      //         stops: [0.0, 1.0],
+      //         tileMode: TileMode.clamp,
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      appBar: AppBar(
+        title: Text('Main Screen'),
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controllerGoogleMap.complete(controller);
+              newGoogleMapController = controller;
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
